2f86e44cb9c234a3c3983cdd0a220d823af62611
GencoMattia <gencomattia@hotmail.it>
2024-07-01 15:37:15 +0200

    added axios and js with script.js

--- index.php
+++ index.php
@@ -13,6 +13,17 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <div id="app">
+
+    </div>
     
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- vue3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- script.js -->
+    <script src="./js/script.js"></script>
 </body>
 </html>
