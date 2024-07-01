5217c52f35789a719a08bde1f322279b94721c8d
GencoMattia <gencomattia@hotmail.it>
2024-07-01 16:22:54 +0200

    attempted to get the list with a API get from the toDoList.json file

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        message: 'Hello Vue!'
+            toDoList: [],
         }
+    },
+
+    methods: {
+        getToDoList() { 
+            axios.get('../json/toDoList.json', {
+                params: {
+                    
+                }
+            })
+            .then(response => {
+                this.toDoList = response.data;
+            })
+            .catch(error => {
+                console.error("Errore durante il recupero della lista:", error);
+            })
+        }   
+    },
+
+    mounted() {
+        this.getToDoList();
     }
 }).mount('#app')
